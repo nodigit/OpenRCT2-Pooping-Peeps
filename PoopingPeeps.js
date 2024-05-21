cce7b9d8f4fbb98ab18721fcc3c5776fd959a055
nodigit <the360tech@aim.com>
2024-05-20 23:55:43 -0400

    Update PoopingPeeps.js to v1.1
    Fixed an issue with the game speed/tick and added monthly alerts

--- PoopingPeeps.js
+++ PoopingPeeps.js
@@ -1,44 +1,69 @@
+var currentMonth = date.month;
+var incidents = 0;
 
 var checkPoopLevel = function() {
+	if (date.ticksElapsed % 40 === 0){
 	
-	var allGuests = map.getAllEntities("guest");
-	
-	for (var i = 0; i < allGuests.length; i++) {
-		var entity = allGuests[i];
+		var allGuests = map.getAllEntities("guest");
 		
-	if (entity.toilet < 255){ entity.toilet = entity.toilet+1;}
+		for (var i = 0; i < allGuests.length; i++) {
+			var entity = allGuests[i];
 			
+			if (entity.toilet < 255){ entity.toilet++;}
+			//if (entity.getFlag("toilet") == false) {entity.setFlag("toilet", true);}
 
-		if (entity.toilet == 255){
+			if (entity.toilet == 255){
 
-			//Peep will randomy defecate once they reach their limit
-			if (Math.random()*200 < 1){
-				var entityPos = {x: entity.x, y: entity.y};
-				var guestsHere = map.getAllEntitiesOnTile("guest", entityPos);
-				var newLitter = map.createEntity("litter", guestsHere[0]);
-				newLitter.litterType = "burger_box";
-				entity.toilet = 0;
-				entity.happiness = 0;
+				//Peep will randomy defecate once they reach their limit
+				if (Math.random()*100 < 1){
+					var entityPos = {x: entity.x, y: entity.y};
+					var guestsHere = map.getAllEntitiesOnTile("guest", entityPos);
+					var newLitter = map.createEntity("litter", guestsHere[0]);
+					newLitter.litterType = "burger_box";
+					entity.toilet = 0;
+					entity.happiness = 0;
+					incidents++;
+				}
+				
 			}
-			
+				
 		}
-			
 	}
+}
 
+var poopReport = function() {
+	if (date.month !== currentMonth){
+
+		//Alerts you if there's too many guests defecating
+		if (incidents >= (park.guests*0.5)+1){
+			var msg = {type: "guests", text: incidents+" pooping incidents occurred in your park last month. Considering adding more toilets to your park."};
+			park.postMessage(msg);
+		}
+		
+		incidents = 0;
+		currentMonth = date.month;
+	}
 }
 
 
-var main = function()
-{
-	context.setInterval(function() {checkPoopLevel();}, 500);
-}
+var main = function() {
+	
+	context.subscribe("interval.day", poopReport);
+	
+	context.subscribe(
+		"interval.tick",
+		function() {
+			checkPoopLevel();
+		}
+	);
+	
+};
 
 registerPlugin({
 	name: "Pooping Peeps",
-	version: "1.0",
+	version: "1.1",
 	authors: ["nodigit"],
 	type: "local",
 	licence: "MIT",
-	minApiVersion: 29,
 	main: main
 });
